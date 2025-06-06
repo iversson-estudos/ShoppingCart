3b06e52b6b5cff486a96208df0d73339f55c56ab
iversson-estudos <i versson.estudos@gmail.com>
2025-06-06 09:26:24 -0300

    google icons imported and displaying now on main page

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -6,7 +6,13 @@ export default function MainPage(){
     return(
         <div className={styles.sidebar}>
             <Link className={styles.homepageButton} to='/'>
-                <img src="/src/assets/images/homepageButton.png"/>
+                <span class="material-symbols-outlined">home</span>
+            </Link>
+            <Link className={styles.shoppingButton} to='/'>
+                <span class="material-symbols-outlined">search</span>
+            </Link>
+            <Link className={styles.homepageButton} to='/'>
+                <span class="material-symbols-outlined">shopping_cart</span>
             </Link>
         </div>
     )
