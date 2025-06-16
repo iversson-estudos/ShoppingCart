36546fc405f408c2beb698f97604cdd008e5535d
iversson-estudos <i versson.estudos@gmail.com>
2025-06-16 08:08:08 -0300

    set function to localForage and function to add new items to cart

--- src/data/shoppingCart.jsx
+++ src/data/shoppingCart.jsx
@@ -1,11 +1,30 @@
 import localforage from "localforage";
 
-export default async function loadCart(){
-
+export  async function loadCart(){
 try{    
     const cart = await localforage.getItem('cart');
     return cart;
 }catch(err){
     console.log(err);
+}}
+
+
+export  async function addItemToCart(itemId,quantity){
+    let cart = await loadCart();
+    let index = cart.findIndex(item=>item.id===itemId);
+    if(index>-1){
+        cart[index] = {itemId,quantity};
+    }else{
+        cart.push({itemId,quantity});
+    }
+    await set(cart);
+    return true;
 }
+
+
+
+
+
+function set(cart){
+    return localforage.setItem('cart',cart);
 }
